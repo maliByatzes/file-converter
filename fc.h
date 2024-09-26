8e14204d53efe2cce77240ae8adeac2563d11748
maliByatzes <malib2027@gmail.com>
2024-09-26 12:40:16 +0200

    remove dead code

--- fc.h
+++ fc.h
@@ -6,17 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// new API
 void convertEpubToPDF(char *file_path, char *result_filename);
 void convertPDFToEpub(char *file_path, char *result_filename);
-
-/*
-int confirmFileExtension(char *filepath, const char *expected_ext);
-char *getFileName(char *filepath);
-void extractEPUBFile(char *filepath, char *dir_path);
-int confirmEPUBFileType(char *dir_path);
-char *parseXMLFile(char *filepath);
-char *getLocationOfPackageDocument(char *dir_path);
-*/
 
 #endif
--- main.c
+++ main.c
@@ -24,42 +24,5 @@ int main(int argc, char **argv) {
   // pdf -> epub
   // convertPDFToEpub(file_path, result_filename);
 
-  /*
-  memset(&temp, 0, sizeof(temp));
-  strncpy(temp, filepath, TEMP_SIZE);
-
-  // make sure the input file is an .epub file
-  if ((confirmFileExtension(temp, EPUB_EXT)) < 0) {
-    fprintf(stderr, "error: unsupported file extension.\n");
-    exit(EXIT_FAILURE);
-  }
-
-  // exctract .epub
-  memset(&temp, 0, sizeof(temp));
-  strncpy(temp, filepath, TEMP_SIZE);
-  char *filename = getFileName(temp);
-  char dir_path[512];
-  snprintf(dir_path, sizeof(dir_path), "./files/%s-dir", filename);
-
-  extractEPUBFile(filepath, dir_path);
-
-  // confirm the mimetype file contents
-  memset(&temp, 0, TEMP_SIZE);
-  strncpy(temp, dir_path, TEMP_SIZE);
-  int ret = confirmEPUBFileType(temp);
-  if (ret < 0) {
-    perror("confirm file type error");
-    exit(EXIT_FAILURE);
-  } else if (ret > 0) {
-    exit(EXIT_FAILURE);
-  }
-
-  memset(&temp, 0, TEMP_SIZE);
-  strncpy(temp, dir_path, TEMP_SIZE);
-  strncat(temp, "/META-INF/container.xml", 24);
-
-  getLocationOfPackageDocument(temp);
-
-  */
   exit(EXIT_SUCCESS);
 }
