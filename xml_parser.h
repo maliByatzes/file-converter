b4d1ccf05ce9c1bddc2d6ba46621141622633fbe
maliByatzes <malib2027@gmail.com>
2024-09-26 16:31:48 +0200

    scaffold xml parser

--- xml_parser.h
+++ xml_parser.h
@@ -6,7 +6,24 @@
 #include <string.h>
 #include <unistd.h>
 
-struct s_xmlparser {};
+struct s_attribute {
+  char *name;
+  char *value;
+};
+
+struct s_element {
+  char *name;
+  struct s_attribute *attributes;
+  int is_prolog;
+};
+
+struct s_xmlparser {
+  size_t position;
+  size_t line;
+  struct s_element *elements;
+};
 typedef struct s_xmlparser XMLParser;
+
+struct s_element *parseContent();
 
 #endif
